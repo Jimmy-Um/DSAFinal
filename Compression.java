3b4f922a69e2093efccc764aa924d2d5639cf3c9
porky3O <zhuraymond158@gmail.com>
2024-12-13 18:56:52 -0800

    calculateNumberOfWords() and initializeOutputFile() methods

--- Compression.java
+++ Compression.java
@@ -13,6 +13,13 @@ import java.util.*;
 public class Compression {
         /* ArrayList String type that saves all new String */
         private static ArrayList<String> dic = null;
+        /* how many words in the input file */
+        private static int numWordsInInputFile = 0;
+        /* how many lines in the input file */
+        private static int numLinesInInputFile = 0;
+        /* Output File and Data Stream */
+        private static FileOutputStream fos = null;
+        private static DataOutputStream dos = null;
 
         /* print all String in Dictionary */
         private static void printDictionary() {
@@ -34,5 +41,52 @@ public class Compression {
                 }
             }
             return i;
+        }
+        private static void calculateNumberOfWords(String fileName) {
+            Scanner scInputFile = null;
+            Scanner scInputLine = null;
+            try {
+                scInputFile = new Scanner(new File(fileName));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            while (scInputFile.hasNextLine()) {
+                scInputLine = new Scanner(scInputFile.nextLine());
+                while (scInputLine.hasNext()) {
+                    String s = scInputLine.next();
+                    numWordsInInputFile++;
+                }
+                numWordsInInputFile++;
+                numLinesInInputFile++;
+            }
+            numWordsInInputFile--;
+            scInputLine.close();
+            scInputFile.close();
+        }
+        private static void initializeOutputFile(String fileName) {
+            File compressedFile = new File(fileName);
+            if (compressedFile.delete() == true) {
+                System.out.println(fileName + " exists. deleting now. We will crate new one");
+            }
+            else {
+                System.out.println(fileName + " does not exist. We will create new one");
+
+            }
+            try {
+                fos = new FileOutputStream(fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                dos = new DataOutputStream(fos);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            /* first 4-bytes is number of words in input file */
+            try {
+                dos.writeInt(numWordsInInputFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 }
